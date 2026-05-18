206abd44191e2b32661d7051a9425754f33259bc
rilinde13 <rilinde@amrod.co.za>
2026-05-18 23:23:29 +0200

    adding practice work

---
+++ Python Homework/lesson 1 practice.py
@@ -0,0 +1,14 @@
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+height = float(input('Enter your height: '))
+print()
+
+print(f'Your name is {name}, you are {age} years old and your height is {height} in meters')
+print()
+
+print(1999 + 100)
+print()
+
+num1 = 1.62
+num2 = 100
+print(f'multiplication: {num1 * num2}')
